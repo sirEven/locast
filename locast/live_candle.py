cc810a3240a0d2565eed4414374a89c1c21ff7c9
sirEven <sir_even@icloud.com>
2024-08-05 18:39:22 +0200

    Clean up and add some conceptual notes.

--- locast/live_candle.py
+++ locast/live_candle.py
@@ -16,19 +16,34 @@ import threading
 from typing import Any, Dict, List
 
 from dydx_v4_client.indexer.socket.websocket import IndexerSocket, CandlesResolution  # type: ignore
-from dydx_v4_client.network import TESTNET  # type: ignore
 
 from locast.candle.candle import Candle
-from locast.candle.dydx.dydx_resolution import DydxResolution
 from locast.candle.exchange import Exchange
 from locast.candle.exchange_candle_mapper import ExchangeCandleMapper
 
 logging.basicConfig(level=logging.DEBUG)
 
+"""
+The idea of this part of the LOCAST component is to have the live candle turn on at the begining of execution, either by itself within init, or by the glue project
+that calls its start function. And stop function at shut down. 
+What can be considered unnecessary is dynamic subscribing and unsubscribing to certain markets - instead we could pause
+publishing those candles if desired. But even that might be unnecessary. We should think about what exactly we would want to 
+pause, once a position has beend opened. Naturally we would keep candles being created and published but would simply stop forming fresh 
+predictions.
+Also we should update (or create) all candle clusters of the corresponding markets and resolutions that we also start as live candles.
+The orchestration of this would be implemented in glue project, meaning Locast only has very rudimentary API methods:
+- create cluster
+- update cluster
+- start live candle
+- stop live candle
 
-# TODO: Refine such that:
-# - In test() we try out another co-routine that we can call along .connect in asyncio.gather(), to simulate different independent components.
-# - Finally implement subscribe and unsubscribe functinos to individually subscribe and unsubscribe to/from markets
+This guarantees, that it can be reused in model training project as well where different glue code is needed but same basic functionality.
+For example: create / update clusters, train models. Whereas glue project would want to just start a live candle and feed it into prediction module.
+Later on maybe have glue code updated to incorporate some sort of regularily update clusters (every n resolutions), retrain models etc, such that prediction module always
+loads the latest trained model before performing predictions.
+"""
+
+
 class DydxV4LiveCandle:
     def __init__(
         self,
@@ -39,6 +54,7 @@ class DydxV4LiveCandle:
         self._exchange = Exchange.DYDX_V4
         self._resolutions = resolutions_for_markets
         self._market_candles: Dict[str, List[Candle]] = {}
+        self.subscriptions: Dict[str, bool] = {}
 
     async def connect(self) -> None:
         # NOTE: ._ws.connect() is a blocking async function call that needs to be wrapped
@@ -59,14 +75,22 @@ class DydxV4LiveCandle:
         if candles := self._market_candles.get(market):
             return candles[1] if len(candles) > 1 else None
 
-    def subscribe_to_candles(self, market: str, resolution: str):
+    def subscribe(self, market: str, resolution: str):
         print(f"Subscribing to {resolution} candles for {market}.")
         res = self._map_to_client_resolution(resolution)
         self._ws.candles.subscribe(market, res)
 
+    def unsubscribe(self, market: str, resolution: str) -> None:
+        res = self._map_to_client_resolution(resolution)
+        self._ws.candles.unsubscribe(market, res)
+
     def _subscribe_to_all_candles(self):
         for market, res in self._resolutions.items():
-            self.subscribe_to_candles(market, res)
+            self.subscribe(market, res)
+
+    def _set_subscription_state(self, channel_id: str, state: bool) -> None:
+        market = channel_id.split("/")[0]
+        self.subscriptions[market] = state
 
     def handle_message(self, ws: IndexerSocket, message: Dict[str, Any]):
         if message["type"] == "connected":
@@ -74,6 +98,11 @@ class DydxV4LiveCandle:
 
         if message["type"] == "subscribed":
             print(f"Subscription successful ({message['channel']}, {message['id']}).")
+            self._set_subscription_state(message["id"], True)
+
+        if message["type"] == "unsubscribed":
+            print(f"Unsubscription successful ({message['channel']}, {message['id']}).")
+            self._set_subscription_state(message["id"], False)
 
         if message["type"] == "channel_batch_data":
             if candle_dict := message["contents"][0]:
@@ -93,11 +122,15 @@ class DydxV4LiveCandle:
                 if active_needs_update:
                     self._update_active_candle(new_candle)
 
-        # DEBUG PRINTS
-        if active_candle := self.get_active_candle("ETH-USD"):
-            print(f"Active candle started at: {active_candle.started_at}")
-        if active_candle := self.get_newest_ended_candle("ETH-USD"):
-            print(f"Newest candle started at: {active_candle.started_at}\n")
+                # DEBUG PRINTS
+                if active_candle := self.get_active_candle(market):
+                    print(
+                        f"Active {market} candle started at: {active_candle.started_at}"
+                    )
+                if active_candle := self.get_newest_ended_candle(market):
+                    print(
+                        f"Newest {market } candle started at: {active_candle.started_at}\n"
+                    )
 
     def _begin_new_active_candle(self, market: str, new_candle: Candle) -> None:
         # shift active (at 0) to newest ended (at 1) by inserting the new candle at 0 as new active
@@ -124,30 +157,3 @@ class DydxV4LiveCandle:
             return CandlesResolution[key]
         else:
             raise ValueError(f"Invalid resolution: {dydx_resolution_notation}.")
-
-
-# This simulates a different component trying to query the live candle for data
-async def price_query(live_candle: DydxV4LiveCandle) -> None:
-    print("debug, entering price_query!")
-    while True:
-        if candle := live_candle.get_newest_ended_candle("ETH-USD"):
-            print(f"CLOSE: {candle.close}")
-
-        await asyncio.sleep(5)
-
-
-async def test():
-    live_candle = DydxV4LiveCandle(
-        host_url=TESTNET.websocket_indexer,
-        resolutions_for_markets={
-            "ETH-USD": DydxResolution.ONE_MINUTE.notation,
-            "BTC-USD": DydxResolution.FIVE_MINUTES.notation,
-        },
-    )
-    live_candle.start_live_candle_connection()
-
-    tasks = [asyncio.create_task(price_query(live_candle))]
-    await asyncio.gather(*tasks)
-
-
-asyncio.run(test())
--- locast/main.py
+++ locast/main.py
@@ -1,18 +1,58 @@
 import asyncio
+from typing import List
+from locast.live_candle import DydxV4LiveCandle
+from dydx_v4_client.network import TESTNET  # type: ignore
+from locast.candle.dydx.dydx_resolution import DydxResolution
 
 
-async def print_some_stuff(name: str) -> None:
-    print(f"Start printing stuff for {name}.")
-    dots = "."
-    for count in range(10):
-        print(f"Stuff {count}{dots}")
-        dots += "."
-        await asyncio.sleep(0.5)
-    print(f"Finish printing stuff for {name}.")
+live_candle = DydxV4LiveCandle(
+    host_url=TESTNET.websocket_indexer,
+    resolutions_for_markets={
+        "ETH-USD": DydxResolution.ONE_MINUTE.notation,
+        "BTC-USD": DydxResolution.ONE_MINUTE.notation,
+    },
+)
 
 
+# This simulates a different component trying to query the live candle for data
+async def price_query(live_candle: DydxV4LiveCandle, markets: List[str]) -> None:
+    print("debug, entering price_query!")
+    while True:
+        for market in markets:
+            if live_candle.subscriptions.get(market):
+                if candle := live_candle.get_newest_ended_candle(market):
+                    print(f"{market} CLOSE: {candle.close}")
+
+        await asyncio.sleep(5)
+
+
+async def unsubscribe_after_delay(
+    live_candle: DydxV4LiveCandle,
+    market: str,
+    delay_sec: int,
+) -> None:
+    await asyncio.sleep(delay_sec)
+    print(f"Unsubscribing from {market}.")
+    live_candle.unsubscribe(market, DydxResolution.ONE_MINUTE.notation)
+
+
+async def subscribe_after_delay(
+    live_candle: DydxV4LiveCandle, market: str, delay_sec: int
+) -> None:
+    await asyncio.sleep(delay_sec)
+    print(f"Subscribing to {market}.")
+    live_candle.subscribe(market, DydxResolution.ONE_MINUTE.notation)
+
+
+# TODO: Would it be possible to have the tasks List be managed globally in order to add/remove tasks dynamically?
 async def main():
-    await print_some_stuff("Severin")
+    live_candle.start_live_candle_connection()
+    tasks = [
+        asyncio.create_task(price_query(live_candle, ["ETH-USD", "BTC-USD"])),
+        asyncio.create_task(unsubscribe_after_delay(live_candle, "ETH-USD", 80)),
+        asyncio.create_task(subscribe_after_delay(live_candle, "ETH-USD", 140)),
+    ]
+    await asyncio.gather(*tasks)
 
 
 asyncio.run(main())
