566aca628fa743de96cb056bc1cff071eff8de2b
sirEven <sir_even@icloud.com>
2024-08-17 00:46:56 +0200

    Change some fixtures in confest and remove unnecessary and unwanted conftest imports.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,15 @@
-from typing import AsyncGenerator
+from decimal import Decimal
+from typing import AsyncGenerator, Generator
+import pytest
 import pytest_asyncio
 
+from sir_utilities.date_time import now_utc_iso
+
+
+from locast.candle.candle import Candle
 from locast.candle.dydx.dydx_resolution import DydxResolution
+from locast.candle.exchange import Exchange
+from locast.candle.resolution import ResolutionDetail
 from locast.candle_fetcher.dydx.api_fetcher.dydx_v4_fetcher import DydxV4Fetcher
 from locast.candle_fetcher.dydx.candle_fetcher.dydx_v4_candle_fetcher import (
     DydxV4CandleFetcher,
@@ -13,10 +21,6 @@ from dydx_v4_client.network import TESTNET, make_mainnet  # type: ignore
 
 from tests.helper.candle_mockery.v4_indexer_mock import V4IndexerClientMock
 
-ETH_USD = "ETH-USD"
-BTC_USD = "BTC-USD"
-LINK_USD = "LINK-USD"
-ONE_MIN = DydxResolution.ONE_MINUTE
 
 MAINNET = make_mainnet(
     rest_indexer="https://indexer.dydx.trade/",
@@ -25,9 +29,52 @@ MAINNET = make_mainnet(
 )
 
 
+@pytest.fixture
+def eth_usd() -> str:
+    return "ETH-USD"
+
+
+@pytest.fixture
+def btc_usd() -> str:
+    return "BTC-USD"
+
+
+@pytest.fixture
+def link_usd() -> str:
+    return "LINK-USD"
+
+
+@pytest.fixture
+def one_min() -> ResolutionDetail:
+    return DydxResolution.ONE_MINUTE
+
+
+@pytest.fixture
+def dummy_candle() -> Generator[Candle, None, None]:
+    yield Candle(
+        None,
+        Exchange.DYDX_V4,
+        "ETH-USD",
+        DydxResolution.ONE_MINUTE.seconds,
+        now_utc_iso(),
+        Decimal("100"),
+        Decimal("100"),
+        Decimal("100"),
+        Decimal("100"),
+        Decimal("100"),
+        10,
+        Decimal("100"),
+        Decimal("100"),
+    )
+
+
 @pytest_asyncio.fixture  # type: ignore
-async def dydx_v4_live_candle() -> AsyncGenerator[DydxV4LiveCandle, None]:
-    markets_per_resolution = {ONE_MIN.notation: [ETH_USD, BTC_USD]}
+async def dydx_v4_live_candle(
+    eth_usd: str,
+    btc_usd: str,
+    one_min: ResolutionDetail,
+) -> AsyncGenerator[DydxV4LiveCandle, None]:
+    markets_per_resolution = {one_min.notation: [eth_usd, btc_usd]}
     live_candle = DydxV4LiveCandle(
         host_url=TESTNET.websocket_indexer,
         markets_per_resolution=markets_per_resolution,
