5acf3974800fc5a390b99849a2fe3c62b98b13c6
sirEven <sir_even@icloud.com>
2024-08-20 15:57:00 +0200

    Add a few tests for CandleUtility.

--- tests/candle/test_candle_utility.py
+++ tests/candle/test_candle_utility.py
@@ -1,7 +1,10 @@
+from typing import List
+import pytest
 from sir_utilities.date_time import now_utc_iso
 
 from locast.candle.candle import Candle
 from locast.candle.candle_utility import CandleUtility as uc
+from tests.conftest import dummy_candle
 
 
 # TODO: test all functions of CandleUtility
@@ -33,3 +36,55 @@ def test_is_newest_valid_candles_returns_false(dummy_candle: Candle) -> None:
 
     # then
     assert uc.is_newest_valid_candle(candle) is False
+
+
+def test_assert_chronological_order_returns_true(
+    dydx_v4_eth_one_min_mock_candles: List[Candle],
+) -> None:
+    # given
+    candles = dydx_v4_eth_one_min_mock_candles
+
+    # when & then
+    uc.assert_chronologic_order(candles)
+
+
+def test_assert_chronological_order_returns_false(
+    dydx_v4_eth_one_min_mock_candles: List[Candle],
+) -> None:
+    # given
+    candles = dydx_v4_eth_one_min_mock_candles
+    faulty_candles = [candles[0], candles[1], candles[3], candles[4]]
+
+    # when & then
+    with pytest.raises(AssertionError):
+        uc.assert_chronologic_order(faulty_candles)
+
+
+def test_assert_candle_unity_returns_true(
+    dydx_v4_eth_one_min_mock_candles: List[Candle],
+) -> None:
+    # given
+    candles = dydx_v4_eth_one_min_mock_candles
+
+    # when & then
+    uc.assert_candle_unity(candles)
+
+
+def test_assert_candle_unity_returns_false(
+    dydx_v4_eth_one_min_mock_candles: List[Candle],
+) -> None:
+    # given
+    candles = dydx_v4_eth_one_min_mock_candles
+    candles[5].market = "BTC-USD"
+
+    # when & then
+    with pytest.raises(AssertionError):
+        uc.assert_candle_unity(candles)
+
+
+@pytest.mark.parametrize("candles", [[], [dummy_candle]])
+def test_assert_candle_unity_does_not_raise(candles: List[Candle]) -> None:
+    # given
+    too_few_candles = candles
+    # when & then
+    uc.assert_candle_unity(too_few_candles)
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,12 @@ from typing import AsyncGenerator, Generator
 import pytest
 import pytest_asyncio
 
-from sir_utilities.date_time import now_utc_iso
-
+from sir_utilities.date_time import now_utc_iso, string_to_datetime
 
 from locast.candle.candle import Candle
 from locast.candle.dydx.dydx_resolution import DydxResolution
 from locast.candle.exchange import Exchange
+from locast.candle.exchange_candle_mapper import ExchangeCandleMapper
 from locast.candle.resolution import ResolutionDetail
 from locast.candle_fetcher.dydx.api_fetcher.dydx_v4_fetcher import DydxV4Fetcher
 from locast.candle_fetcher.dydx.candle_fetcher.dydx_v4_candle_fetcher import (
@@ -16,10 +16,15 @@ from locast.candle_fetcher.dydx.candle_fetcher.dydx_v4_candle_fetcher import (
 )
 from locast.live_candle.dydx.dydx_live_candle import DydxV4LiveCandle
 
+from locast.candle.candle_utility import CandleUtility as cu
+
 from dydx_v4_client.indexer.rest.indexer_client import IndexerClient  # type: ignore
 from dydx_v4_client.network import TESTNET, make_mainnet  # type: ignore
 
 from tests.helper.candle_mockery.v4_indexer_mock import V4IndexerClientMock
+from tests.helper.candle_mockery.mock_dydx_v4_candle_dicts import (
+    mock_dydx_v4_candle_dict_batch,
+)
 
 
 MAINNET = make_mainnet(
@@ -66,6 +71,30 @@ def dummy_candle() -> Generator[Candle, None, None]:
         Decimal("100"),
         Decimal("100"),
     )
+
+
+@pytest.fixture
+def dydx_v4_eth_one_min_mock_candles() -> Generator[list[Candle], None, None]:
+    start_str = "2024-01-01T00:00:00+00:00"
+    end_str = "2024-01-05T00:00:00+00:00"
+    exchange = Exchange.DYDX_V4
+    market = "ETH-USD"
+    resolution = DydxResolution.ONE_MINUTE.seconds
+
+    start_str = "2024-01-01T00:00:00+00:00"
+    end_str = "2024-01-05T00:00:00+00:00"
+    start = string_to_datetime(start_str)
+    end = string_to_datetime(end_str)
+    amount = cu.amount_of_candles_in_range(start, end, resolution)
+    eth_dicts = mock_dydx_v4_candle_dict_batch(
+        DydxResolution.ONE_MINUTE.notation,
+        market,
+        start_str,
+        end_str,
+        batch_size=amount,
+    )
+
+    yield ExchangeCandleMapper.to_candles(exchange, eth_dicts)
 
 
 @pytest_asyncio.fixture  # type: ignore
