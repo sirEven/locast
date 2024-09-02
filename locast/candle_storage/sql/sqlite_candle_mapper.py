acb24610b37e7c7cf8e62421814e2fc0e8bc3d96
sirEven <sir_even@icloud.com>
2024-09-02 19:44:52 +0200

    Add TODO...

--- locast/candle_storage/sql/sqlite_candle_mapper.py
+++ locast/candle_storage/sql/sqlite_candle_mapper.py
@@ -2,6 +2,7 @@ from locast.candle_storage.database_candle_mapper import DatabaseCandleMapper
 from locast.candle_storage.sql.sqlite_candle_mapping import SqliteCandleMapping
 
 
+# TODO: I think we can ditch this one...
 class SqliteCandleMapper(DatabaseCandleMapper):
     def __init__(self, mapping: SqliteCandleMapping) -> None:
         super().__init__(mapping)
