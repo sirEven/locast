1cfce8d68cac652c219e6c1c7fd5ff19fe7ab6fa
sirEven <sir_even@icloud.com>
2024-09-05 20:16:05 +0200

    Add another test.

--- tests/store_manager/test_store_manager.py
+++ tests/store_manager/test_store_manager.py
@@ -5,7 +5,7 @@ from locast.candle.candle_utility import CandleUtility as cu
 from locast.candle.exchange import Exchange
 from locast.candle.resolution import ResolutionDetail, Seconds
 from locast.candle_storage.sql.sqlite_candle_storage import SqliteCandleStorage
-from locast.store_manager.store_manager import StoreManager
+from locast.store_manager.store_manager import ExistingClusterException, StoreManager
 
 
 @pytest.mark.asyncio
@@ -38,3 +38,21 @@ async def test_create_cluster_results_in_correct_cluster_state(
     assert cluster_info.is_uptodate
     assert cluster_info.amount == expected_amount
     assert cluster_info.tail.started_at == start_date
+
+
+@pytest.mark.asyncio
+async def test_create_cluster_throws_error(
+    store_manager_mock_memory: StoreManager,
+) -> None:
+    # given a cluster is already present
+    manager = store_manager_mock_memory
+
+    market = "ETH-USD"
+    resolution = ResolutionDetail(Seconds.FOUR_HOURS, "4HOURS")
+
+    start_date = string_to_datetime("2024-08-01T00:00:00.000Z")
+    await manager.create_cluster(market, resolution, start_date)
+
+    # when the same cluster is created, then exception is thrown
+    with pytest.raises(ExistingClusterException):
+        await manager.create_cluster(market, resolution, start_date)
